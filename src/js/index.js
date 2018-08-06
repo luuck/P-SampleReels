ef40de6a30b969fadb238d98d0db001cce3aacbf
luuck <Singapo91@163.com>
2018-08-06 15:56:22 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,14 @@
 $(function() {
-
     function handle() {
         $('.J-header').animate({ 'opacity': 1, 'filter': 'alpha(opacity=100)' }).show();
-
-        var data = {
-            list: [{
-                title: '公共接口快速生成文档',
-                gitHubUrl: 'https://github.com/luuck/P-FastCreateInterfaceDoc',
-                linkUrl: 'https://luuck.github.io/P-FastCreateInterfaceDoc/dist/view/index.html#/',
-                imgUrl: 'https://luuck.github.io/P-FastCreateInterfaceDoc/doc/pic/intro.png',
-                desc: '这是一款通内置公共接口，快速生成Markdown语法的工具。'
-            }]
-        };
+        var data = dataConfig; // dataconfig.js
         var html = template('tpl-list', data);
-        document.getElementById('article-list').innerHTML = html;
+        $('#article-list').html(html);
     }
-
-    preloadimages(['https://luuck.github.io/P-SampleReels/dist/img/top-bg.jpeg']).done(function() {
+    preloadimages(['./img/top-bg.jpeg']).done(function() {
         handle();
+        lazyload(function() {
+            $('.J-footer').animate({ 'opacity': 1, 'filter': 'alpha(opacity=100)' }).show();
+        }); // lazyload.js
     });
-
 });
