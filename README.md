0d3494b9b343b0c9bb38f0002b4ea45987c06395
luuck <Singapo91@163.com>
2018-08-06 16:15:13 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # P-SampleReels
 
-Personal Sample Reels
+Personal Sample Reels 
+
+### 安装
+
+``` bash
+# 打包
+gulp build
+
+# 删除dist目录下的所有内容
+gulp clean
+```
--- dist/js/main-v1.0.0.js
+++ dist/js/main-v1.0.0.js
@@ -33,7 +33,7 @@ $(function() {
         var html = template('tpl-list', data);
         $('#article-list').html(html);
     }
-    preloadimages(['./img/top-bg.jpeg']).done(function() {
+    preloadimages(['./img/top-bg-1.jpeg']).done(function() {
         handle();
         lazyload(function() {
             $('.J-footer').animate({ 'opacity': 1, 'filter': 'alpha(opacity=100)' }).show();
